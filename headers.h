68b80b9d58e0c185cf1f7b96f4774e85d2d9e699
vkalyanam <vkalyanam@lantronix.com>
2018-04-23 14:33:20 +0530

    Adding Header file and Knowledge document.

---
+++ headers.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>     /* memcpy bzero */
+#include <sys/socket.h> /* socket connect */
+#include <netinet/in.h> /* struct sock */
+#include <arpa/inet.h>  /* helper functions inet */
